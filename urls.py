163ae33dfbac2a1ebcf83a898ef45daa49fc743d
Aaron Alexander <nerdfiles@gmail.com>
2012-02-10 01:27:53 -0600

    cleanup admin

--- urls.py
+++ urls.py
@@ -5,13 +5,13 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
 
 if settings.DEBUG:
     urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        url(r'^_assets/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         url(r'', include('django.contrib.staticfiles.urls')),
     ) + urlpatterns
