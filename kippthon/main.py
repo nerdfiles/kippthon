a6dfbbb326476631cddafbeb7f5cdaa4bfee0991
Aaron Alexander <nerdfiles@gmail.com>
2012-03-22 16:32:28 -0500

    cleanup

--- kippthon/main.py
+++ kippthon/main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 import pycurl
 from pprint import pprint
 import simplejson
 import cStringIO
 import urllib
 import urllib2
+import sys
 
 buf = cStringIO.StringIO()
 
@@ -17,11 +20,30 @@ try:
 except ImportError:
   pass
 
-def kippt():
-  url = 'https://kippt.com/api/lists/'
+def search(q=''):
+  url = 'https://kippt.com/api/search/clips/?limit=5&q=%s' % q
+  req = urllib2.Request(url=url)
+  req.add_header('X-Kippt-Username', USER)
+  req.add_header('X-Kippt-API-Token', API_KEY)
+  r = urllib2.urlopen(req)
+  obj = simplejson.loads(r.read())
+  #pprint( obj )
+  for item in obj['objects']:
+    print item['title']
+
+def lists():
+  url = 'https://kippt.com/api/lists/?offset=0&limit=2'
   # provide @prop data for post
   req = urllib2.Request(url=url)
   req.add_header('X-Kippt-Username', USER)
   req.add_header('X-Kippt-API-Token', API_KEY)
   r = urllib2.urlopen(req)
   obj = simplejson.loads(r.read())
+  pprint( obj )
+
+if (len(sys.argv) > 1):
+  search(q=sys.argv[1])
+else:
+  lists()
+
+
